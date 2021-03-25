2f345344d9d3a3c24b038e5757882ebc090a330d
Ulyana Mayakova <u_mayakova@mail.ru>
2021-03-25 11:13:56 +0300

    View binding added

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,30 +2,25 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.core.widget.ImageViewCompat
+import ru.netology.nmedia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val icon: ImageView = findViewById(R.id.icon)
-        val groupName: TextView = findViewById(R.id.groupName)
-        val date: TextView = findViewById(R.id.date)
-        val postText: TextView = findViewById(R.id.postText)
-        val likesCount: TextView = findViewById(R.id.likesCount)
-        val sharesCount: TextView = findViewById(R.id.sharesCount)
-        val viewsCount: TextView = findViewById(R.id.viewsCount)
-
-        icon.setImageResource(R.drawable.ic_launcher_netology_foreground)
-        groupName.text = "Нетология. Университет интернет-профессий"
-        date.text = "24 марта 16:00"
-        postText.text = "Очень большой и очень интересный текст про что-то безумно интересное и невообразимо крутое"
-        likesCount.text = "4"
-        sharesCount.text = "5"
-        viewsCount.text = "200"
+        binding.icon.setImageResource(R.drawable.ic_launcher_netology_foreground)
+        binding.groupName.text = "Нетология. Университет интернет-профессий"
+        binding.date.text = "24 марта 16:00"
+        binding.postText.text = "Очень большой и очень интересный текст про что-то безумно интересное и невообразимо крутое"
+        binding.likesCount.text = "4"
+        binding.sharesCount.text = "5"
+        binding.viewsCount.text = "200"
 
     }
 }
