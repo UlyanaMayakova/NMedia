3ba0ec48db26aeb54d5aa34146c47b9a7d41dccd
Ulyana Mayakova <u_mayakova@mail.ru>
2021-04-02 18:45:27 +0300

    Number logic added

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -6,21 +6,46 @@ import ru.netology.nmedia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        binding.icon.setImageResource(R.drawable.ic_launcher_netology_foreground)
-        binding.groupName.text = "Нетология. Университет интернет-профессий"
-        binding.date.text = "24 марта 16:00"
-        binding.postText.text = "Очень большой и очень интересный текст про что-то безумно интересное и невообразимо крутое"
-        binding.likesCount.text = "4"
-        binding.sharesCount.text = "5"
-        binding.viewsCount.text = "200"
+        val post = Post(
+                id = 1,
+                groupName = "Нетология. Университет интернет-профессий",
+                date = "24 марта 16:00",
+                postText = "Очень большой и очень интересный текст про что-то безумно интересное и невообразимо крутое",
+                likesCount = 9_221,
+                sharesCount = 1_100,
+                viewsCount = 1_200,
+                likedByMe = false
+        )
+
+        with(binding) {
+            icon.setImageResource(R.drawable.ic_launcher_netology_foreground)
+            groupName.text = post.groupName
+            date.text = post.date
+            postText.text = post.postText
+            likesCount.text = post.numbersStyle(post.likesCount)
+            sharesCount.text = post.numbersStyle(post.sharesCount)
+            viewsCount.text = post.numbersStyle(post.viewsCount)
+
+            like.setOnClickListener {
+                post.likedByMe = !post.likedByMe
+                if (post.likedByMe) {
+                    like.setImageResource(R.drawable.ic_liked)
+                    likesCount.text = post.numbersStyle(++post.likesCount)
+                } else {
+                    like.setImageResource(R.drawable.ic_like)
+                    likesCount.text = post.numbersStyle(--post.likesCount)
+                }
+            }
+
+            share.setOnClickListener{
+                    sharesCount.text = post.numbersStyle(++post.sharesCount)
+            }
+        }
 
     }
 }
